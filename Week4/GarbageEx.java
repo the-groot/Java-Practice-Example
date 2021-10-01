5d8ad9bf22f170157324d25f140993904597fbd1
the-groot <sangwoo0795@naver.com>
2021-10-01 15:17:14 +0900

    연습문제 4-8~4-12 업로드

---
+++ Week4/GarbageEx.java
@@ -0,0 +1,16 @@
+package Week4;
+
+public class GarbageEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a=new String("Good");
+		String b=new String("Bad");
+		String c=new String("Normal");
+		String d,e;
+		a=null;
+		d=c;
+		c=null;
+	}
+
+}
