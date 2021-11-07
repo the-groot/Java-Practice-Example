e8e98a45adba708e5bded6dcdd8763900a4d7c10
the-groot <sangwoo0795@naver.com>
2021-11-07 16:22:59 +0900

    최종본 업로드

--- Week8/practice12.java
+++ Week8/practice12.java
@@ -1,5 +1,80 @@
 package Week8;
 
-public class practice12 {
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.Vector;
 
+public class practice12 {
+	
+	String path=System.getProperty("user.dir");
+	String filename=null;
+	String key=null;
+	Scanner sc=new Scanner(System.in);
+	FileReader fr;
+	Vector<String> vector=new Vector<>();
+	Vector<Integer> linenumber=new Vector<>();
+	
+	practice12() {
+		System.out.println("전체 경로명이 아닌 파일 이름만 입력하는 경우, 파일은 프로젝트 폴더에 있어야 합니다.");
+		path=path+"\\src\\Week8\\";
+		System.out.print("대상 파일명 입력>> ");
+		filename=sc.nextLine();
+		path=path+filename;		//path 최신화
+		
+		while(true) {
+		
+		System.out.print("검색할 단어나 문장>> ");
+		key=sc.nextLine();
+		if(key.equals("그만"))
+			break;
+		
+		
+		
+		try{
+			
+			fr=new FileReader(path);	//파일 객체 생성
+			Scanner sc2=new Scanner(fr);	//scanner로 한줄씩입력받을 것임
+			
+			while(sc2.hasNext()) {
+				String line=sc2.nextLine();
+				vector.add(line);
+			}
+			sc2.close();
+			
+			for(int i=0; i<vector.size(); i++) {
+				String line=vector.get(i);
+				if(line.indexOf(key)!=-1)		//검색하는 키가있을때
+					linenumber.add(i);	//라인넘버 추가
+			}
+			
+			for(int i=0; i<linenumber.size(); i++) {
+				int linenum=linenumber.get(i);
+				String line=vector.get(linenum);
+				System.out.println(linenum+":\t"+line);
+			}
+			
+			linenumber.clear();
+			vector.clear();
+		
+		}catch(IOException e) {
+			System.out.println("입출력 오류");
+		}
+		
+		}
+		sc.close();
+	}
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		new practice12();
+		
+	}
+	
 }
+
+
--- Week8/practice14.java
+++ Week8/practice14.java
@@ -5,87 +5,137 @@ import java.io.*;
 
 public class practice14 {
 	
-	public static void FileSearch() {
-		String src="C:\\";
-		String path;
+	String path="C:\\";	//경로명저장
+	File current;	//현재 위치의 파일을 받을 객체
+	File[] subFiles;	//파일을 하나씩 받을버퍼
+	String user_input;
+	String[] key=new String[3];
+	Scanner sc=new Scanner(System.in);
+	
+
+	void setFiles() {
+		current=new File(path);
+		subFiles=current.listFiles();
+	}
+	
+	void run() {
 		
-		Scanner sc=new Scanner(System.in);
-		File current=null;		//현재있는 위치
-		File[] subfiles=null;	//현재위치의 파일들
-		current=new File(src);		//경로할당
-		subfiles=current.listFiles();	//파일리스트할당
+		System.out.println("["+current.getAbsolutePath()+"]");	//현재경로출력
 		
+		for(File f : subFiles) {
+			if(f.isDirectory())		//디렉터리면
+			System.out.print("dir");	//dir출력
+			else if(f.isFile())	//파일이면
+			System.out.print("\tfile");	//파일출력
+			System.out.print("\t"+f.length()+"바이트");
+			System.out.print("\t"+f.getName());
+			System.out.println();
+		}
 		
-		while(true) {	
-			
-		for(File one : subfiles) {	//파일하나씩받아서
-										//정보출력
-			if(one.isDirectory()) {		//디렉토리면
-				System.out.print("dir\t");
-			}
-			else if(one.isFile()) {
-				System.out.print("file\t");
-			}
-			
-			System.out.println(one.length()+"바이트\t"+one.getName());
-			
-			}
 		System.out.print(">>");
-		path=sc.next();
-		String array[]=path.split(" ");
 		
-		if(path.equals("..")) {
-			String s=current.getParent();
+		user_input=sc.nextLine();	//명령어 입력받음
+		
+		key=user_input.split(" ");	//명령어를 띄어쓰기를 기준으로 분할함
+		if(key.length==1) {	//명령어가 한개라면 path 변경이나 프로그램종료이므로
+			setPath();	//setpath호출
+		}
+		else {		//명령어 두개이상이면
+			setCommand();	// setcommand호출
+		}
+		
+	}
+	
+	void setPath()
+	{	
+		 if(key[0].equals("그만")) {		//그만이면 종료
+			System.out.println("프로그램을 종료합니다.");
+			return;
+		}
+		
+		String checkpath=path;		//경로명존재하는지확인하는 경로명변수
+		checkpath=checkpath+"\\"+key[0];
+		File check=new File(checkpath);		//경로명이 존재하는지확인
+		System.out.println(path);
+		if(check.exists()==false) {	//존재하지않으면
+			System.out.println("경로명이 존재하지 않습니다.");
+			run();
+		}
+		
+		if(key[0].equals("..")) {		//..을 입력했을때
 			
-			if(s==null) {		//	부모노드가 없으면
-								//루트노드이므로 이동할수없음
-				System.out.println("이동할수없습니다.");
-				continue;	//src변경없음
+			if(current.getParent()==null) {	//현재 경로가 루트노드일때는
+				System.out.println("현재 루트노드이므로 이동할 수 없습니다");
+				run();
+			}
+			
+			else {	
+				path=current.getParent();	//path 부모로 변경
+				setFiles();		//file다시설정 후
+				run();		//런
+			}
+			
+		}
+		
+		
+		else {		//정상적인 하위라면
+		path=path+"\\"+key[0];		//경로명 재설정
+		setFiles();
+		run();		//다시 run 호출
+		}
+	}
+	
+	void setCommand() {
+		
+		if(key.length==2) {		//명령어가 두개일때
+			if(key[0].equals("mkdir")) {	//명령어가 mkdir일때
+				File newdir=new File(current,key[1]);
+				
+				if(newdir.exists()) {	//이미존재하면
+					System.out.println("이미 존재하는 디렉터리입니다.");
+				}
+				else {
+					System.out.println(key[1]+"디렉터리를 생성하였습니다.");
+					newdir.mkdir();
+				}
+			}
+			else if(key[0].equals("rename")) {
+				System.out.println("두개의 파일명이 주어지지 않았습니다.");
+			}
+			
+			setFiles();
+			run();
+			
+		}
+		else if(key.length==3) {
+			File srcfile=new File(current,key[1]);
+			File newfile=new File(current,key[2]);
+			
+			if(srcfile.exists()) {	//바꿀파일명이존재하고
+				if(newfile.exists()) {	//새롭게바꿀이름의 파일명이존재하면
+					System.out.println("이미"+key[2]+"파일이 존재합니디");
+				}	//오류문구 출력
+				
+				else {	//바꿔줌
+					srcfile.renameTo(newfile);	//파일변경
+					System.out.println(key[1]+"을 "+key[2]+" 이름 변경하였습니다. ");
+				}
 			}
 			else {
-				current=new File(current.getParent());		//경로할당
-				subfiles=current.listFiles();	//파일리스트할당
+				System.out.println("파일이 존재하지않습니다.");
 			}
-		}
-		else if(path.equals("그만"))
-			break;
-		else {
-			if(array.length==1) {	//length가 하나일때
 			
-			current=new File(src);		//경로할당
-			subfiles=current.listFiles();	//파일리스트할당
-			}
-			else if(array.length==2) {
-				if(array[0].equals("mkdir")) {	//mkdir이 입력되면
-					File newDir=new File(current,array[1]);
-					
-					if(newDir.exists())
-					System.out.println("이미 존재하는 파일이나 디렉터리 이름입니다.");
-					
-					else {
-						System.out.println(array[1]+" 디렉터리를 생성하였습니다.");
-						newDir.mkdir();
-					}
-				}
-				
-				else if(array[0].equals("rename")) {
-					System.out.println("두개의 파일명이 주어지지않았습니다!");
-				}
-			}
-			else {
-				
-				
-				
-				
-			}
+			setFiles();
+			run();
 		}
-		src=src+path+"\\";
-		System.out.println(src);
-		}		
+		
 		
 	}
 	
 	public static void main(String[] args) {
-		FileSearch();
+		practice14 p=new practice14();
+		System.out.println("***** 파일 탐색기입니다. *****");
+		p.setFiles();
+		p.run();
 	}
 }
